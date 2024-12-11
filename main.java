04d88095da7c53bc7471b0d029f7adce75e67e79
codinome <146401049+codinomello@users.noreply.github.com>
2024-12-11 00:19:21 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main{
+
+  public static void main(String[] args){
+    System.out.print("Olá, Mundo!\n");
+
+    // comentário
+
+    /*
+     * comentário multilinha
+     */
+
+    int número = 1;
+    double decimal = 1.23;
+    boolean humano = true;
+    char símbolo = '@';
+    String nome = "Gabriel";
+  }
+}
